6a1a6d125552c0cdb687e66001201cf4308a512b
stoff1180 <stoffmustapha1180@gmail.com>
2024-03-09 11:33:37 -0800

    fixed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-    Fabric script that generates tgz archive from contents of web_static
+    Fabric script generating tgz archive from contents of web_static
 """
 from fabric.api import local
 from datetime import datetime
@@ -8,15 +8,14 @@ from datetime import datetime
 
 def do_pack():
     """
-        generates a .tgz archine from contents of web_static
+       Generates a .tgz archive from contents of web_static
     """
     time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
     file_name = "versions/web_static_{}.tgz".format(time)
-    result = local("mkdir -p ./versions", capture=True)
-    if result.failed:
+    try:
+        local("mkdir -p ./versions")
+        local("tar --create --verbose -z --file={} ./web_static"
+              .format(file_name))
+        return file_name
+    except:
         return None
-    result = local("tar --create --verbose -z --file={} \
-            ./web_static".format(file_name), capture=True)
-    if result.failed:
-        return None
-    return file_name
