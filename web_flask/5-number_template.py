546d90df04c2c328e726bb7aaa1140a3e502145b
stoff1180 <stoffmustapha1180@gmail.com>
2024-03-22 05:16:14 -0700

    Committing untracked files before merge

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+   Sript that starts a Flask web application
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbn():
+    """
+        returns Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+        returns HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_var(text):
+    """
+        displays the value of text variable passed in
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def text_var_python(text="is cool"):
+    """
+        displays the value of text variable "is cool"
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def var_num(n):
+    """
+        displays a variable, but only if an integer
+    """
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def var_num_template(n):
+    """
+        displays number in html page
+    """
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
